606e008816fa7dff7f91a3b097faefec7908c0c7
zorin <sorin_matias@yahoo.com>
2017-10-04 15:07:05 +0300

    primele 3 probleme

---
+++ aPersonalizedEcho.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#sed -r 's/(.*)/Welcome \1/g'
+
+#or
+
+#or
+read name
+echo "Welcome $name"
---
+++ loopingAndSkipping.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for x in {1..99..2}
+do
+	echo $x
+done
+
+#or
+
+#echo -e 1 "\n"{3..99..2} \
+
+#or
+
+#seq 1 2 99 \
